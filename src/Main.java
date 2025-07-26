2855d054a892f94515b0275058cf5b6f0d2ce4bf
Galileo <pellizer.galileo16@gmail.com>
2025-07-26 17:42:50 +0200

    MPJ Init

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import mpi.MPI;
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    static final int NUM_POINTS_TO_HEAT = 2;
+    static final int HEIGHT = 5;
+    static final int WIDTH = 5;
+
+    public static void main(String[] args) {
+        MPI.Init(args);
+
+        System.out.println("test");
+
+        MPI.Finalize();
     }
 }
